f74a7f985d32d9997650f3cd53a13896b8ff0ecd
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2022-06-08 13:47:43 -0700

    recreated cleaning data function

--- huachinango_functions.R
+++ huachinango_functions.R
@@ -1,2 +1,42 @@
 #' huachinango_functions
-#' @author 
+#' @author Hem Nalini Morzaria-Luna
+#' @description 
+
+clean_files <- function(thisnofile, beneficios.csv, tipo.beneficio){
+  
+  print(thisnofile)
+  thisfile <- beneficios.csv[thisnofile]
+  print(thisfile)
+  
+  thisbeneficio <-  tipo.beneficio[thisnofile]
+  print(thisbeneficio)
+  
+  this.data <- read_csv(here("inputs",thisfile), local = locale(encoding = "latin1")) %>% 
+    janitor::clean_names() 
+  
+  thesenames <- names(this.data)
+  
+  if(any(thesenames=="pesqueria")){
+    
+   rev.data <- this.data %>% 
+    dplyr::select(ejercicio, cve_inegi, rnpa, monto_federal_aportado, pesqueria) %>% 
+    dplyr::mutate(beneficio = thisbeneficio, 
+                  pesqueria = tolower(pesqueria),
+                  cve_inegi = as.character(cve_inegi))
+          
+  }
+  
+  if(!any(thesenames=="pesqueria")){
+    
+    rev.data <- this.data %>% 
+      dplyr::select(ejercicio, cve_inegi, rnpa, monto_federal_aportado) %>% 
+      dplyr::mutate(beneficio = thisbeneficio,
+                    pesqueria = NA_character_,
+                    cve_inegi = as.character(cve_inegi))
+      
+    
+  }
+ 
+   print(rev.data)
+  return(rev.data) 
+}
---
+++ libraries.R
@@ -0,0 +1,20 @@
+
+.packages = c("tidyverse","devtools","readxl",
+              "sp","sf","rgdal",
+              "rnaturalearth","RCurl","XML",
+              "here", "raster", "exactextractr",
+              "Redmonder","gridExtra","corrplot","mgcViz","ggspatial",
+              "ggrepel","patchwork","png","grid","janitor","purrr")
+
+# Install CRAN packages (if not already installed)
+.inst <- .packages %in% installed.packages()
+
+if(length(.packages[!.inst]) > 0) install.packages(.packages[!.inst], )
+
+# Load packages into session 
+lapply(.packages, require, character.only=TRUE)
+
+remotes::install_github("iobis/robis")
+
+# Load packages into session 
+lapply(.packages, require, character.only=TRUE)
